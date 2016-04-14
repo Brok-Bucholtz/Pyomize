d6fb221cea54c1b2610f5efe66983feff8e75f23
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-04-14 11:45:39 -0700

    Make component extraction a helper function

---
+++ analyze/helper.py
@@ -0,0 +1,38 @@
+def extract_commit_components(commits):
+    commit_components = {
+        'messages': [],
+        'repositories': [],
+        'emails': [],
+        'dates': [],
+        'changed_files': []
+    }
+
+    for commit in commits:
+        commit_components['messages'].append(commit.message)
+        commit_components['repositories'].append(commit.repository_name)
+        commit_components['emails'].append(commit.committer_email)
+        commit_components['dates'].append(commit.date)
+        commit_components['changed_files'].append(len(commit.files))
+
+    return commit_components
+
+
+def extract_commit_file_components(commit_files):
+    commit_file_components = {
+        'patches': [],
+        'additions': [],
+        'changes': [],
+        'deletions': [],
+        'filenames': [],
+        'statuses': []
+    }
+
+    for commit_file in commit_files:
+        commit_file_components['patches'].append(commit_file.patch)
+        commit_file_components['additions'].append(commit_file.additions)
+        commit_file_components['changes'].append(commit_file.changes)
+        commit_file_components['deletions'].append(commit_file.deletions)
+        commit_file_components['filenames'].append(commit_file.filename)
+        commit_file_components['statuses'].append(commit_file.status)
+
+    return commit_file_components
--- analyze/stats.py
+++ analyze/stats.py
@@ -3,6 +3,7 @@ from github_database import engine, Commit, File
 from sqlalchemy.orm import sessionmaker
 from numpy import mean
 from collections import Counter
+from analyze.helper import extract_commit_components, extract_commit_file_components
 
 
 def get_word_frequency(lines):
@@ -27,59 +28,31 @@ def run():
         .query(File) \
         .all()
 
-    commit_stats = {
-        'messages': [],
-        'repositories': [],
-        'emails': [],
-        'dates': [],
-        'changed_files': []
-    }
-    commit_file_stats = {
-        'patches': [],
-        'additions': [],
-        'changes': [],
-        'deletions': [],
-        'filenames': [],
-        'statuses': []
-    }
-
-    for commit in commits:
-        commit_stats['messages'].append(commit.message)
-        commit_stats['repositories'].append(commit.repository_name)
-        commit_stats['emails'].append(commit.committer_email)
-        commit_stats['dates'].append(commit.date)
-        commit_stats['changed_files'].append(len(commit.files))
-
-    for commit_file in commit_files:
-        commit_file_stats['patches'].append(commit_file.patch)
-        commit_file_stats['additions'].append(commit_file.additions)
-        commit_file_stats['changes'].append(commit_file.changes)
-        commit_file_stats['deletions'].append(commit_file.deletions)
-        commit_file_stats['filenames'].append(commit_file.filename)
-        commit_file_stats['statuses'].append(commit_file.status)
+    commit_components = extract_commit_components(commits)
+    commit_file_components = extract_commit_file_components(commit_files)
 
     print "Commit Patch Word Frequency"
-    print get_word_frequency(commit_stats['messages'])
+    print get_word_frequency(commit_components['messages'])
     print "Commit Repo Frequency"
-    print get_string_frequency(commit_stats['repositories'])
+    print get_string_frequency(commit_components['repositories'])
     print "Committer Email Frequency"
-    print get_string_frequency(commit_stats['emails'])
+    print get_string_frequency(commit_components['emails'])
     # Get Freq of dates
     print "Mean of File Changes in a Commit"
-    print mean(commit_stats['changed_files'])
+    print mean(commit_components['changed_files'])
     print "\n"
 
     print "File Path Word Frequency"
-    print get_word_frequency([patch for patch in commit_file_stats['patches'] if patch])
+    print get_word_frequency([patch for patch in commit_file_components['patches'] if patch])
     print "Mean of Additions for a File"
-    print mean(commit_file_stats['additions'])
+    print mean(commit_file_components['additions'])
     print "Mean of Changes for a File"
-    print mean(commit_file_stats['changes'])
+    print mean(commit_file_components['changes'])
     print "Mean of Deletions for a File"
-    print mean(commit_file_stats['deletions'])
+    print mean(commit_file_components['deletions'])
     # Do something with filename
     print "File Status Frequency"
-    print get_string_frequency(commit_file_stats['statuses'])
+    print get_string_frequency(commit_file_components['statuses'])
 
 
 if __name__ == "__main__":

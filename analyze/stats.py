2a52956ad97ee51b72b1d479bcb9ff42a98c9dd9
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-04-15 17:54:45 -0700

    Add stat for first word in a commit message

--- analyze/stats.py
+++ analyze/stats.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from numpy import mean
 from collections import Counter
 from analyze.helper import extract_commit_components, extract_commit_file_components
+from re import compile
 
 
 def _get_word_frequency(lines):
@@ -53,10 +54,25 @@ def _print_general_stats(sample_size=100):
     # Do something with filename
     print "File Status Frequency"
     print _get_string_frequency(commit_file_components['statuses'])
+    print "\n"
+
+
+def _print_first_word_commit_message_frequency():
+    db_session = sessionmaker(bind=engine)()
+    commits = db_session \
+        .query(Commit) \
+        .all()
+    regex_search = compile('^([\s]+)?([^\s]+)')
+
+    commit_message_first_words = [regex_search.match(commit.message).group().strip() for commit in commits]
+
+    print "Commit Message First Word Frequency"
+    print _get_word_frequency(commit_message_first_words)
 
 
 def run():
     _print_general_stats()
+    _print_first_word_commit_message_frequency()
 
 if __name__ == "__main__":
     run()

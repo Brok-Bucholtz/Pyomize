5b40273d7b323eb0fe942a22edab273f76dab93c
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-04-19 22:38:07 -0700

    Show accuracy for each label

--- analyze/predict.py
+++ analyze/predict.py
@@ -3,7 +3,7 @@ from analyze.helper import get_first_word, remove_non_alpha
 from github_database import engine, Commit
 from sklearn.svm import SVC
 from sklearn.cross_validation import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 from sklearn.preprocessing import LabelEncoder
 from analyze.dimension_reduction import filter_merge_commits, get_top_frequent_words, filter_data_by_labels, stem_labels
 
@@ -39,11 +39,21 @@ def run():
     num_train = num_all*0.25
     clf = SVC()
     X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, train_size=(num_train / float(num_all)))
+    clf.fit(X_train, y_train)
 
     print "Training set size: {}".format(len(X_train))
-    clf.fit(X_train, y_train)
-    print "Accuracy for training set: {}".format(accuracy_score(y_train, clf.predict(X_train)))
-    print "Accuracy for test set: {}".format(accuracy_score(y_test, clf.predict(X_test)))
+    print "Accuracy for training set:"
+    print classification_report(
+            y_train,
+            clf.predict(X_train),
+            labels=label_encoder.transform(top_words),
+            target_names=top_words)
+    print "Accuracy for test set:"
+    print classification_report(
+            y_test,
+            clf.predict(X_test),
+            labels=label_encoder.transform(top_words),
+            target_names=top_words)
 
 if __name__ == "__main__":
     run()

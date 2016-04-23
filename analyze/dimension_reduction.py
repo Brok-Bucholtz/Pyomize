b18e407e7688453cbe05af86cfc585144327f247
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-04-23 08:32:38 -0700

    Create function for neural network commit data

--- analyze/dimension_reduction.py
+++ analyze/dimension_reduction.py
@@ -56,15 +56,14 @@ def get_file_stats_data(label_count=8):
     return X_all, y_all
 
 
-def run():
+def get_file_patch_data():
     db_session = sessionmaker(bind=engine)()
     commits = db_session \
         .query(Commit) \
-        .all()
-    commit_files = db_session \
-        .query(File) \
+        .options(joinedload(Commit.files)) \
         .all()
 
+    commit_files_list = [commit.files for commit in commits]
     commit_ignore_words = [u'to', u'the', u'for', u'and', u'in', u'from', u'of']
     python_keywords = [u'and', u'del', u'from', u'not', u'while', u'as', u'elif', u'global', u'or', u'with', u'assert',
                        u'else', u'if', u'pass', u'yield', u'break', u'except', u'import', u'print', u'class', u'exec',
@@ -75,16 +74,6 @@ def run():
     commit_vectorizer.fit_transform([commit.message for commit in commits])
 
     commit_file_vectorizer = TfidfVectorizer(stop_words=commit_file_ignore_words)
-    commit_file_vectorizer.fit_transform([commit_file.patch for commit_file in commit_files if commit_file.patch])
+    commit_file_vectorizer.fit_transform([commit_file.patch for commit_file in commit_files_list if commit_file.patch])
 
-    print sorted(dict(zip(
-        commit_vectorizer.get_feature_names(),
-        commit_vectorizer.idf_)).items(),
-        key=itemgetter(1))
-    print sorted(dict(zip(
-        commit_file_vectorizer.get_feature_names(),
-        commit_file_vectorizer.idf_)).items(),
-        key=itemgetter(1))
-
-if __name__ == "__main__":
-    run()
+    return commit_vectorizer, commit_file_vectorizer
